4dbebaf54c4f4672b26fbf0c15834096dbb7c518
Sachin Aggarwal <sachinaggarwal@Sachins-MacBook-Air.local>
2018-01-06 13:40:34 +0530

    Adding the assertions and removing normal ifelse

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,10 +1,13 @@
 const utils = require('./utils');
+const expect = require('expect');
+
 it('should add two numbers',()=>{
   var res = utils.add(33,11);
 
-  if(res!== 44){
-    throw new Error(`Expected 44, but got ${res}`); //this function is used to fail the test. See that if you make addition wrong in utils.js i.e. do a+b+3 => test will fail
-  }
+  // if(res!== 44){
+  //   throw new Error(`Expected 44, but got ${res}`); //this function is used to fail the test. See that if you make addition wrong in utils.js i.e. do a+b+3 => test will fail
+  // }
+  expect(res).toBe(44);
 });
 
 it('should square',()=>{
