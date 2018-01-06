40e0ac28e183ac04ec0878515a90f4aa89d3be5a
Sachin Aggarwal <sachinaggarwal@Sachins-MacBook-Air.local>
2018-01-06 14:05:15 +0530

    Add various assertion methods use

--- utils/utils.js
+++ utils/utils.js
@@ -1,3 +1,10 @@
 module.exports.add = (a,b) => a+b;
 
 module.exports.square = (x) => x*x;
+
+module.exports.setName = (user, fullname) => {
+  var names = fullname.split(' ');
+  user.firstname = names[0];
+  user.lastname = names[1];
+  return user;
+};
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -7,13 +7,26 @@ it('should add two numbers',()=>{
   // if(res!== 44){
   //   throw new Error(`Expected 44, but got ${res}`); //this function is used to fail the test. See that if you make addition wrong in utils.js i.e. do a+b+3 => test will fail
   // }
-  expect(res).toBe(44);
+  expect(res).toBe(44).toBeA('number');
 });
 
 it('should square',()=>{
   var res = utils.square(4);
 
-  if(res!== 16){
-    throw new Error(`Expected 44, but got ${res}`); //this function is used to fail the test. See that if you make addition wrong in utils.js i.e. do a+b+3 => test will fail
-  }
+  expect(res).toBe(16).toBeA('number');
+});
+
+it('should expect some value',()=>{
+  // expect(12).toNotBe(12);
+  // expect({name:'Andrew'}).toBe({name:'Andrew'}); this will fail because these are not equal => these are 2 diff objects with same prop
+  // expect({name:'Andrew'}).toEqual({name:'Andrew'}); //this checks if objects are equal valuewise, same to be done for arrays as they are also objects
+  // expect([2,3,4]).toInclude(5);
+  // expect([1,2,3]).toExclude(5);
+  expect({name:'Andrew', age:25}).toInclude({age:25});
+
+
+});
+
+it('should return correct names',()=>{
+  expect(utils.setName({},'Sachin Aggarwal')).toEqual({firstname:'Sachin',lastname:'Aggarwal'});
 });
