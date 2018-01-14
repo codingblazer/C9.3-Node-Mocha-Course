975fb44622d8686e3165289089c2634d995f5902
codingblazer <sasachin04@gmail.com>
2018-01-14 23:05:55 +0530

    Spies on actual code

--- spies/app.test.js
+++ spies/app.test.js
@@ -11,4 +11,20 @@ describe('App',()=>{
     spy('Andrew',25);
     expect(spy).toHaveBeenCalledWith('Andrew',25);
   });
+
+  var db = {
+    saveUser : expect.createSpy()
+  };
+
+  app.__set__('db',db);
+
+  it('should call saveUser with user object',()=>{
+    var email = 'sasachin04@gmail.com';
+    var password = '123abc';
+
+    app.handleSignUp(email,password);
+
+    expect(db.saveUser).toHaveBeenCalledWith({email,password});
+  });
+
 });
